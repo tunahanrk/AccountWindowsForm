e313f0430130df04e8bd57e0bb55430e90b669a6
Tunahan ARIK <76400173+tunahanrk@users.noreply.github.com>
2023-05-13 21:05:43 +0300

    Added files

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Acc_Name_textBox = new System.Windows.Forms.TextBox();
+            this.Acc_No_textBox = new System.Windows.Forms.TextBox();
+            this.ADD_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Acc_Name_textBox
+            // 
+            this.Acc_Name_textBox.Location = new System.Drawing.Point(155, 52);
+            this.Acc_Name_textBox.Name = "Acc_Name_textBox";
+            this.Acc_Name_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Acc_Name_textBox.TabIndex = 0;
+            // 
+            // Acc_No_textBox
+            // 
+            this.Acc_No_textBox.Location = new System.Drawing.Point(155, 107);
+            this.Acc_No_textBox.Name = "Acc_No_textBox";
+            this.Acc_No_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Acc_No_textBox.TabIndex = 1;
+            // 
+            // ADD_button
+            // 
+            this.ADD_button.Location = new System.Drawing.Point(294, 81);
+            this.ADD_button.Name = "ADD_button";
+            this.ADD_button.Size = new System.Drawing.Size(75, 23);
+            this.ADD_button.TabIndex = 2;
+            this.ADD_button.Text = "button1";
+            this.ADD_button.UseVisualStyleBackColor = true;
+            this.ADD_button.Click += new System.EventHandler(this.ADD_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ADD_button);
+            this.Controls.Add(this.Acc_No_textBox);
+            this.Controls.Add(this.Acc_Name_textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Acc_Name_textBox;
+        private System.Windows.Forms.TextBox Acc_No_textBox;
+        private System.Windows.Forms.Button ADD_button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Account[] ACC_ARR = new Account[100];
+        public int N;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Acc_Sort()
+        {
+            Account Temp = new Account();
+            int i, j;
+
+            for (i = 1; i < N; N++)
+                for (j = 0; j < N - i; j++)
+                    if (ACC_ARR[j].Acc_No < ACC_ARR[j + 1].Acc_No)
+                    {
+                        Temp = ACC_ARR[j];
+                        ACC_ARR[j] = ACC_ARR[j + 1];
+                        ACC_ARR[j + 1] = Temp;
+                    }
+        }
+
+        public void AVG_MAX(ref double AVG, ref double MAX)
+        {
+            int i;
+            AVG = 0;
+            for (i = 0; i < N; i++)
+                AVG = AVG + ACC_ARR[i].Acc_Balance;
+
+            AVG = AVG / N;
+
+            MAX = ACC_ARR[0].Acc_Balance;
+            for (i = 1; i < N; i++)
+                if (ACC_ARR[i].Acc_Balance > MAX) MAX = ACC_ARR[i].Acc_Balance;
+        }
+
+        public int Acc_no_Search(int Acc_No_to_Search_for)
+        {
+            int ret = -1;
+            for (int i = 0; i < N; i++)
+                if (ACC_ARR[i].Acc_No == Acc_No_to_Search_for)
+                    ret = i;
+            return ret;
+        }
+        private void ADD_button_Click(object sender, EventArgs e)
+        {
+            int Acc_No;
+            int.TryParse(Acc_No_textBox.Text, out Acc_No);
+
+            ACC_ARR[N] = new Account(Acc_Name_textBox.Text, Acc_No);
+            N++;
+        }
+    }
+
+    public class Account
+    {
+        public string Acc_Name { get; set; }
+        public int Acc_No { get; set; }
+        public DateTime Acc_Date { get; set; }
+        public double Acc_Balance { get; set; }
+
+        public Account()
+        {
+            Acc_Name = "";
+            Acc_No = -1;
+            Acc_Date = DateTime.Now;
+            Acc_Balance = 0;
+        }
+
+        public Account(string Name, int No)
+        {
+            Acc_Name = Name;
+            Acc_No = No;
+            Acc_Date = new DateTime();
+            Acc_Balance = 0;
+        }
+    }
+}
